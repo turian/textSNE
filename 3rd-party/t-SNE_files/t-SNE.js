38db11d4be109a4d17b4be9147ace4a32f2525ff
Joseph Turian <turian@iro.umontreal.ca>
2009-05-11 13:56:56 -0400

    Added t-SNE software

---
+++ 3rd-party/t-SNE_files/t-SNE.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20090120
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('t-SNE_files/t-SNEMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
